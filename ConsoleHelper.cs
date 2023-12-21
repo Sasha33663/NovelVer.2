5b95397dab6f191df8e6107208c30d9fdf3f0354
Евпатий <80116240+Sasha33663@users.noreply.github.com>
2023-12-21 04:34:16 +0300

    Add files via upload

---
+++ ConsoleHelper.cs
@@ -0,0 +1,80 @@
+﻿namespace NovelVer._2;
+
+public partial class Chapter
+{
+    public static class ConsoleHelper
+    {
+        public static void WriteLineGreen(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+
+        public static void WriteLineCyan(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(text);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+
+        public static void WriteLineMagenta(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(text);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+
+        public static void WriteLineGray(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(text);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+
+        public static void WriteLineDarkBlue(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(text);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+
+        public static void WriteLineRed(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+
+        public static void WriteLineYellow(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(text);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+
+        public static void WriteLineDarkMagenta(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine(text);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+
+        public static void WriteLineDarkGray(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine(text);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+    }
+}
+    
